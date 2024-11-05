8994e9e435f5f2e5d6d36588e14ee47f5ba6b33e
DEJVOSS <63589160+IronBeagle2@users.noreply.github.com>
2024-11-05 15:34:54 +0100

    add libinfo

---
+++ LibInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dejvoss.SQLite
+{
+    public class LibInfo
+    {
+        // Dejvoss.SQLite
+        // (c) DEJVOSS Productions 2024
+        // A wrapper for SQLite
+
+        public static String version = "1.0.0-dev";
+    }
+}
